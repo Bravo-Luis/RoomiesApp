2bc3b1fde9a6273c1bf326257976484d7c59368e
Bravo-Luis <luisbravo@ucsb.edu>
2022-09-27 22:08:57 -0700

    Revert "Update ContentView.swift"
    This reverts commit 5f6184257960bd715e1f5aa4030838b529a4ed3e.

--- fridge/fridge/ContentView.swift
+++ fridge/fridge/ContentView.swift
@@ -29,6 +29,7 @@ struct ContentView: View {
             }
             
             
+            
         }
         .fullScreenCover(isPresented: $chatting, content: {
             chatView(presentingChatView: $chatting)
